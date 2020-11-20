19f336c544fa95b5dce7379fdc709bf381dd17c5
chacelancelot <lakalancelot95@gmail.com>
2020-11-20 18:02:21 +0700

    run exe, micros

---
+++ config.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/prometheus/common/log"
+	"github.com/spf13/viper"
+	"strconv"
+	"strings"
+)
+
+const (
+	DefTaskTag           = ""
+	DefTaskIsSignificant = false
+	DefTaskExtensions    = ""
+	DefTaskDetections    = ""
+	DefExportProcesses   = true
+	DefExportATTCKMatrix = true
+)
+
+type AppConfig struct {
+	taskTag           string
+	taskIsSignificant bool
+	taskExtensions    []string
+	taskDetections    []int
+
+	exportProcesses   bool
+	exportATTCKMatrix bool
+}
+
+func ReadAppConfig(configFilePath string) (*AppConfig, error) {
+	viper.SetConfigName(configFilePath)
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Warn("the configuration file not found, switched to default configuration")
+		} else {
+			return nil, fmt.Errorf("failed to read configuration file: %s", err)
+		}
+	}
+	viper.SetDefault("public_tasks.tag", DefTaskTag)
+	viper.SetDefault("public_tasks.significant", DefTaskIsSignificant)
+	viper.SetDefault("public_tasks.extensions", DefTaskExtensions)
+	viper.SetDefault("public_tasks.detections", DefTaskDetections)
+	viper.SetDefault("export.processes", DefExportProcesses)
+	viper.SetDefault("export.ATT&CK_matrix", DefExportATTCKMatrix)
+
+	taskTag := strings.TrimSpace(viper.GetString("public_tasks.tag"))
+	rawTaskExtensions := strings.TrimSpace(viper.GetString("public_tasks.extensions"))
+	rawTaskDetections := strings.TrimSpace(viper.GetString("public_tasks.detections"))
+
+	var taskExtensions []string
+	for _, ext := range strings.Split(rawTaskExtensions, ",") {
+		val, ok := SupportedTaskExtensions[ext]
+		if !ok {
+			return nil, fmt.Errorf("invalid extension '%s': possible values are %s", ext, FormatStrSlice(GetStrMapKeys(SupportedTaskExtensions)))
+		}
+		taskExtensions = append(taskExtensions, val)
+	}
+	var taskDetections []int
+	for _, detection := range strings.Split(rawTaskDetections, ",") {
+		val, ok := SupportedTaskDetections[detection]
+		if !ok {
+			return nil, fmt.Errorf("invalid detection '%s': possible values are %s", detection, FormatStrSlice(GetIntMapKeys(SupportedTaskDetections)))
+		}
+		taskDetections = append(taskDetections, val)
+	}
+	return &AppConfig{
+		taskTag:           taskTag,
+		taskIsSignificant: viper.GetBool("public_tasks.significant"),
+		taskExtensions:    taskExtensions,
+		taskDetections:    taskDetections,
+		exportProcesses:   viper.GetBool("export.processes"),
+		exportATTCKMatrix: viper.GetBool("export.ATT&CK_matrix"),
+	}, nil
+}
+
+func (config *AppConfig) ToTaskParamsJsonQuoted() string {
+	taskParams := &TaskParams{
+		IsPublic:    true,
+		Runtype:     []string{},
+		Verdict:     config.taskDetections,
+		Ext:         config.taskExtensions,
+		Significant: config.taskIsSignificant,
+		Tag:         config.taskTag,
+	}
+	bytes, _ := json.Marshal(taskParams)
+	return strings.Trim(strconv.Quote(string(bytes)), `"`)
+}
---
+++ main.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+	"encoding/json"
+	"strconv"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+var (
+	configFilePath string
+	startTaskIndex uint
+	numOfTasks     uint
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+	log.Logger = log.Output(zerolog.ConsoleWriter{
+		Out:        os.Stdout,
+		TimeFormat: time.RFC3339,
+	})
+	flag.StringVar(&configFilePath, "c", "config.yml", "the `configuration` file")
+	flag.StringVar(&configFilePath, "config", "config.yml", "the `configuration` file")
+	flag.UintVar(&startTaskIndex, "i", 0, "crawl tasks start from the `index`")
+	flag.UintVar(&startTaskIndex, "index", 0, "crawl tasks start from the `index`")
+	flag.UintVar(&numOfTasks, "n", 0, "number of tasks to crawl")
+	flag.UintVar(&numOfTasks, "number", 0, "number of tasks to crawl")
+}
+
+// app.any.run endpoint
+var endpointList = [...]string{
+	"wss://app.any.run/sockjs/399/i73_d8dy/websocket",
+	"wss://app.any.run/sockjs/529/c95zff_m/websocket",
+}
+
+// ghi file
+func dumpToFile(fileName string, bytes []byte) error {
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = file.Write(bytes)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+
+func dumpTask(client *AppAnyClient, malwareTag string, task *RawTask) error {
+	//get task info
+	//var  task2 *RawTask
+	processes, err := client.GetProcesses(task)
+	if err != nil {
+		return err
+	}
+	incidents, err := client.GetIncidents(task)
+	if err != nil {
+		return err
+	}
+
+	ips, err := client.GetIps(task)
+	if err != nil {
+		return err
+	}
+
+	domain, err := client.GetDNSQueries(task)
+	if err != nil {
+		return err
+	}
+
+	httpRequests, err := client.GetHttpRequests(task)
+	if err != nil {
+		return err
+	}
+
+	threats, err := client.GetThreats(task)
+	if err != nil {
+		return err
+	}
+
+	//set task info
+	mainObject := task.Fields.Public.Objects.MainObject
+	processData := &ProcessData{
+		Name:      			mainObject.Names.Basename,
+		Md5:       			mainObject.Hashes.Md5,
+		UUID:      			task.Fields.UUID,
+		Processes: 			processes,
+		Incidents: 			incidents,
+		Ips: 	   			ips,
+		Domain:	   			domain,
+		HttpRequests: 		httpRequests,
+		Threats:            threats,
+	}
+
+	//convert struct to json
+	bytes, err := json.MarshalIndent(processData, "", " ")
+	if err != nil {
+		return err
+	}
+
+	//write json file
+	taskFileName := fmt.Sprintf("%s/%s.json", malwareTag, task.Fields.UUID)
+	if err := dumpToFile(taskFileName, bytes); err != nil {
+		return err
+	}
+	return nil
+}
+
+
+func main() {
+	flag.Parse()
+	appConfig, err := ReadAppConfig(configFilePath)
+	if err != nil {
+		log.Fatal().Err(err).Msgf("failed to parse configuration file '%s'", configFilePath)
+	}
+
+	reqHeader := make(http.Header)
+	reqHeader.Add("Host", "app.any.run")
+	reqHeader.Add("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0")
+	reqHeader.Add("Origin", "https://app.any.run")
+
+	config := &AppAnyClientConfig{
+		Endpoint:  endpointList[rand.Intn(len(endpointList))],
+		ReqHeader: reqHeader,
+		AppConfig: appConfig,
+	}
+	client, err := NewAppAnyClient(config)
+	if err != nil {
+		log.Fatal().Err(err).Msg("in NewAppAnyClient")
+	}
+	if err := client.Connect(); err != nil {
+		log.Fatal().Err(err).Msg("unable to send connect msg")
+	}
+	log.Info().Msg("connected to App.Any.Run")
+
+	totalTaskCount, err := client.GetNumOfTasks()
+	if err != nil {
+		log.Fatal().Err(err).Msg("in GetNumOfTasks")
+	}
+	log.Info().Msgf("Number of possible tasks: %d", totalTaskCount)
+	if startTaskIndex >= totalTaskCount {
+		log.Fatal().Msgf("the requested start index (%d) must not be less than number of tasks available (%d)", startTaskIndex, totalTaskCount)
+	}
+	if numOfTasks == 0 {
+		numOfTasks = totalTaskCount - startTaskIndex
+	}
+	if startTaskIndex+numOfTasks > totalTaskCount {
+		numOfTasks = totalTaskCount - startTaskIndex
+		log.Warn().Msgf("only able to crawl %d tasks", numOfTasks)
+	}
+	log.Info().Msgf("Start crawling tasks (number %d, startIndex: %d)", numOfTasks, startTaskIndex)
+	tasks, err := client.GetTasks(int(numOfTasks), int(startTaskIndex))
+	if err != nil {
+		log.Fatal().Err(err).Msg("in GetProcesses")
+	}
+
+	//create folder
+	var Tag string  = appConfig.taskTag + "-"
+	for _, extension := range appConfig.taskExtensions{
+        Tag = Tag + extension + ","
+	}
+	Tag = Tag + "-"
+	for _, detection := range appConfig.taskDetections{
+        Tag = Tag + strconv.Itoa(detection) 
+	}
+	os.Mkdir(Tag, os.ModePerm)
+
+	//for each task of list task
+	
+	for _, task := range tasks {
+		fmt.Print("\n\n")
+		// print task info
+		log.Info().Msg(task.GetIdentity())
+
+		if err := dumpTask(client, Tag, task); err != nil {
+			log.Info().Msg("cannot dump RawTask, ")
+			client.conn.Close()
+			return
+		}
+	}
+	// for _, task := range tasks {
+	// 	fmt.Print("\n\n")
+	// 	log.Info().Msg(task.GetIdentity())
+	// 	processes, err := client.GetProcesses(task)
+	// 	if err != nil {
+	// 		log.Fatal().Err(err).Msgf("failed to get processes")
+	// 	}
+	// 	incidents, err := client.GetIncidents(task)
+	// 	if err != nil {
+	// 		log.Fatal().Err(err).Msgf("failed to get incidents")
+	// 	}
+	// 	for _, proc := range processes {
+	// 		log.Info().Msgf("[PROCESS] %d - %s - %s", proc.Fields.Pid, proc.Fields.Scores.ImportantReason, proc.Fields.Image)
+	// 	}
+	// 	for _, incident := range incidents {
+	// 		log.Info().Msgf("[MITRE ATT&CK] %s, %v", incident.Fields.Title, incident.Fields.Mitre)
+	// 	}
+	// }
+}
